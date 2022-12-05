0ef5af59ac0109f93b93d692b258a20e1be34bb2
MasadanThe <oskar.nylund@gmail.com>
2022-12-05 14:22:18 +0100

    Added constructor and position variables to class Man

--- src/main/java/com/example/revolutionary_hangman/Man.java
+++ src/main/java/com/example/revolutionary_hangman/Man.java
@@ -1,7 +1,16 @@
 package com.example.revolutionary_hangman;
 
 public class Man {
-    private int height;
     private int width;
+    private int height;
 
+    private int xPosition;
+    private int yPosition;
+
+    public Man (int xPosition, int yPosition, int width, int height){
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.width = width;
+        this.height = height;
+    }
 }
