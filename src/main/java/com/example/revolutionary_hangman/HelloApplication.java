39cf0ece7809e45febb77c5f687c34462f974b1f
MasadanThe <oskar.nylund@gmail.com>
2022-12-05 15:17:51 +0100

    Fixed dimensions for Man and added test to HelloApplication

--- src/main/java/com/example/revolutionary_hangman/HelloApplication.java
+++ src/main/java/com/example/revolutionary_hangman/HelloApplication.java
@@ -2,7 +2,10 @@ package com.example.revolutionary_hangman;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +13,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Man testMan = new Man(0,0, 400, 800);
+        testMan.createDrawing();
+        Group testGroup = new Group();
+        testGroup.getChildren().addAll(testMan.getDrawing());
+        Scene scene = new Scene(testGroup, 1500, 800);
+        scene.setFill(Color.WHITE);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/revolutionary_hangman/Man.java
+++ src/main/java/com/example/revolutionary_hangman/Man.java
@@ -34,7 +34,7 @@ public class Man {
     public void createDrawing(){
         // Create a base
         Rectangle base = new Rectangle();
-        base.setHeight(height * 0.1);
+        base.setHeight(height * 0.05);
         base.setWidth(width * 0.6);
         base.setX( xPosition + width * 0.2);
         base.setY(yPosition + height * 0.9);
@@ -43,17 +43,17 @@ public class Man {
 
         // Create the pole
         Rectangle pole = new Rectangle();
-        pole.setHeight(height * 0.7);
-        pole.setWidth(width * 0.1);
-        pole.setX(base.getX() + base.getWidth() * 0.5);
-        pole.setY(base.getY() + pole.getHeight());
+        pole.setHeight(height * 0.8);
+        pole.setWidth(width * 0.05);
+        pole.setX(base.getX() + base.getWidth() * 0.5 - pole.getWidth() * 0.5);
+        pole.setY(base.getY() - pole.getHeight());
         pole.setFill(Color.BLACK);
         drawing.add(pole);
 
         // Create the roof
         Rectangle roof = new Rectangle();
         roof.setWidth(base.getWidth() * 0.5);
-        roof.setHeight(height * 0.1);
+        roof.setHeight(height * 0.02);
         roof.setX(pole.getX());
         roof.setY(pole.getY());
         roof.setFill(Color.BLACK);
@@ -62,8 +62,8 @@ public class Man {
         // Create stability pole
         Line stabilityPole = new Line();
         stabilityPole.setStartX(pole.getX());
-        stabilityPole.setStartY(pole.getY() + pole.getHeight() * 0.2);
-        stabilityPole.setEndX(roof.getX() + roof.getWidth() * 0.2);
+        stabilityPole.setStartY(pole.getY() + pole.getHeight() * 0.3);
+        stabilityPole.setEndX(roof.getX() + roof.getWidth() * 0.5);
         stabilityPole.setEndY(roof.getY());
         stabilityPole.setFill(Color.BLACK);
         drawing.add(stabilityPole);
@@ -72,16 +72,16 @@ public class Man {
         Rectangle noose = new Rectangle();
         noose.setX(roof.getX() + roof.getWidth());
         noose.setY(roof.getY());
-        noose.setWidth(width * 0.1);
-        noose.setHeight(height * 0.3);
+        noose.setWidth(width * 0.01);
+        noose.setHeight(height * 0.15);
         noose.setFill(Color.BLACK);
         drawing.add(noose);
 
         // Create head
         Circle head = new Circle();
-        head.setRadius(width * 0.3);
+        head.setRadius(Math.min(height * 0.1, width * 0.1));
         head.setCenterX(noose.getX());
-        head.setCenterY(noose.getY() + head.getRadius());
+        head.setCenterY(noose.getY() + head.getRadius() + noose.getHeight());
         head.setFill(Color.BLACK);
         drawing.add(head);
 
@@ -89,44 +89,44 @@ public class Man {
         Rectangle body = new Rectangle();
         body.setX(head.getCenterX());
         body.setY(head.getCenterY() + head.getRadius());
-        body.setHeight(height * 0.5);
-        body.setWidth(width * 0.1);
+        body.setHeight(pole.getHeight() * 0.4);
+        body.setWidth(width * 0.01);
         body.setFill(Color.BLACK);
         drawing.add(body);
 
         // Create left arm
         Line leftArm = new Line();
         leftArm.setStartX(body.getX());
-        leftArm.setStartY(body.getY() + body.getHeight()*0.2);
-        leftArm.setEndX(body.getX() - body.getHeight() * 0.3);
-        leftArm.setEndY(body.getY() + body.getHeight() * 0.3);
+        leftArm.setStartY(body.getY() + body.getHeight() * 0.2);
+        leftArm.setEndX(body.getX() - body.getHeight() * 0.25);
+        leftArm.setEndY(leftArm.getStartY() + body.getHeight() * 0.25);
         leftArm.setFill(Color.BLACK);
         drawing.add(leftArm);
 
         // Create right arm
         Line rightArm = new Line();
         rightArm.setStartX(body.getX());
-        rightArm.setStartY(body.getY() + body.getHeight()*0.2);
-        rightArm.setEndX(body.getX() + body.getHeight() * 0.3);
-        rightArm.setEndY(body.getY() + body.getHeight() * 0.3);
+        rightArm.setStartY(body.getY() + body.getHeight() * 0.2);
+        rightArm.setEndX(body.getX() + body.getHeight() * 0.25);
+        rightArm.setEndY(rightArm.getStartY() + body.getHeight() * 0.25);
         rightArm.setFill(Color.BLACK);
         drawing.add(rightArm);
 
         // Create left leg
         Line leftLeg = new Line();
-        leftLeg.setStartX(body.getX());
+        leftLeg.setStartX(body.getX() + body.getWidth() * 0.5);
         leftLeg.setStartY(body.getY() + body.getHeight());
-        leftLeg.setEndX(leftLeg.getStartX() - body.getHeight() * 0.3);
-        leftLeg.setEndY(leftLeg.getStartY() + body.getHeight() * 0.3);
+        leftLeg.setEndX(leftLeg.getStartX() - body.getHeight() * 0.25);
+        leftLeg.setEndY(leftLeg.getStartY() + body.getHeight() * 0.25);
         leftLeg.setFill(Color.BLACK);
         drawing.add(leftLeg);
 
         // Create right leg
         Line rightLeg = new Line();
-        rightLeg.setStartX(body.getX());
+        rightLeg.setStartX(body.getX() + body.getWidth() * 0.5);
         rightLeg.setStartY(body.getY() + body.getHeight());
-        rightLeg.setEndX(rightLeg.getStartX() + body.getHeight() * 0.3);
-        rightLeg.setEndY(rightLeg.getStartY() + body.getHeight() * 0.3);
+        rightLeg.setEndX(rightLeg.getStartX() + body.getHeight() * 0.25);
+        rightLeg.setEndY(rightLeg.getStartY() + body.getHeight() * 0.25);
         rightLeg.setFill(Color.BLACK);
         drawing.add(rightLeg);
     }
