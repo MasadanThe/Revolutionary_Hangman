a5b2ef8805c9abb434523e4669f667815568eee5
MasadanThe <oskar.nylund@gmail.com>
2022-12-05 14:24:41 +0100

    Added arraylist drawing

--- src/main/java/com/example/revolutionary_hangman/Man.java
+++ src/main/java/com/example/revolutionary_hangman/Man.java
@@ -1,5 +1,10 @@
 package com.example.revolutionary_hangman;
 
+import javafx.scene.shape.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Man {
     private int width;
     private int height;
@@ -7,10 +12,14 @@ public class Man {
     private int xPosition;
     private int yPosition;
 
+    private List<Shape> drawing;
+
     public Man (int xPosition, int yPosition, int width, int height){
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.width = width;
         this.height = height;
+
+        drawing = new ArrayList<>();
     }
 }
