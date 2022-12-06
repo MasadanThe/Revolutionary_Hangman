e0ea0be19f455c2155c77a73c7c009e75b259aeb
Dimitri <reg@dmost.se>
2022-12-06 12:08:09 +0100

    class Player_1

---
+++ src/main/java/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+
+    public static class Man {
+
+
+    private String name;
+    private String word;
+    public int xPosition;
+    public int yPosition;
+    public int width;
+    public int height;
+
+        public Man(String name, String word, int xPosition, int yPosition, int width, int height) {
+            this.name = name;
+            this.word = word;
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
