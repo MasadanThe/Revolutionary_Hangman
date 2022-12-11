6ec6a4e6a77ce4e249cb830d5b50afe3298673a6
MasadanThe <oskar.nylund@gmail.com>
2022-12-11 18:17:57 +0100

    Removed variables

--- src/main/java/com/example/revolutionary_hangman/Hangman.java
+++ src/main/java/com/example/revolutionary_hangman/Hangman.java
@@ -29,10 +29,6 @@ public class Hangman extends Application {
         final int WIDTH = 1500;
         final int HEIGHT = 800;
 
-        int players = 0;
-        int round = 0;
-        // 0 = Choose who to pick, 1 = random who to pick, 2 = mix of them both.
-        int settings = 0;
 
         ArrayList<Player> playersList = new ArrayList<>();
 
